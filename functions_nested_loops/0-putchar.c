4d8dd710fc187ec71b5c6a0e8503c488f181e606
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-19 09:07:59 +0200

    Project 3, Task 0, attempt 1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char text[7] = '_putchar'
+		int i;
+
+	for (i = 0; i <=7; i++)
+	{
+		_putchar(text[i])
+	}
+	return (0);
+}
