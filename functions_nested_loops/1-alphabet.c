786dd97d2a41dbeff628aff7165fba287de1fd55
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-19 10:07:51 +0200

    Project 3, Task 1, attempt 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char text;
+
+	for (text = 'a'; text <= 'z'; text++)
+	{
+		_putchar(text);
+	}
+	_putchar('\n');
+	return (0);
+}
