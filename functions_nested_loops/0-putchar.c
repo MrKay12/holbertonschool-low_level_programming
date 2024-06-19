8400db99ec9268cb1de7db012c6719e21df673a9
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-19 09:25:44 +0200

    Project 3, Task 0, attempt 2 (forget main + compil)

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,12 +9,13 @@
 
 int main(void)
 {
-	char text[7] = '_putchar'
+	char text[9] = "_putchar";
 		int i;
 
-	for (i = 0; i <=7; i++)
+	for (i = 0; i <=9; i++)
 	{
-		_putchar(text[i])
+		_putchar(text[i]);
 	}
+	_putchar('\n');
 	return (0);
 }
