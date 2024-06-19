117a5a1cdd26f12a4d7cfc88a486b559ba3c5913
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-19 11:41:40 +0200

    Project 3, Task 3, attempt 2 (betty correction)

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * _islower(int c) - Entry point
- *
+ * _islower - Entry point
+ * @c: is an input
  * Return: Always 0 (Success)
  */
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	
+
 		return (1)
-	
+
 	else
 		return (0)
 }
