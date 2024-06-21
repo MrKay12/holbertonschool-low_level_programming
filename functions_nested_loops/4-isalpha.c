42697a9308a02547fbceaf88883eef164b4a77ca
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-21 09:17:51 +0200

    Project 3, Task 4, attempt 1

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _isalpha - Entry point
+* @c: is an input
+* Return: Always 0 (Success)
+*/
+
+int _isalpha(int c)
+{
+	for (c = 65; c <= 122; c++)
+	{
+		if (c == 'c' || c == 'C')
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+	_putchar('\n');
+	return (0);
+}
