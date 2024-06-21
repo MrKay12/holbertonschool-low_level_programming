5e0c3a95d29ddcc0b411e401dd8bcf35323bdec2
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-21 15:00:01 +0200

    Project 3, Task 9, attempt 1

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* time_table - Entry point
+*
+* Return: Always 0.
+*/
+
+void time_table(void)
+{
+	int row;
+	int column;
+	int result;
+	int left;
+	int right;
+
+	result = row * column;
+
+	left = result / 10;
+
+	right = result % 10;
+
+	for (column = '0'; column <= '9'; column++)
+	{
+		for (row = '0'; row <= '9'; row++)
+		{
+			if (row * column  <  10)
+			{
+				_putchar('0' + right)
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(left);
+				_putchar(right);
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
