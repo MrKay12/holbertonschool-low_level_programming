1dfef7f424cab61d3eb7f9ba95daa1483d99fcad
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-21 15:18:47 +0200

    Project 3, Task 9, attempt 7

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 			left = result / 10;
 			right = result % 10;
 
-			if (row * column < 10)
+			if (row * column < '9')
 			{
 				_putchar('0' + right);
 				_putchar(',');
