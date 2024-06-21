71db3e5ca709f1403b01af45f175135d2cd1a562
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-21 11:37:58 +0200

    Project 3, Task 8, attempt 1

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* jack_bauer - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+void jack_bauer(void)
+{
+	int m;
+	int h;
+
+	for (h = 0; h <= 23, h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			printf("%i:%i", h, m);
+		}
+	}
+}
