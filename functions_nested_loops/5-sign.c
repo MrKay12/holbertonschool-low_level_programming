51748da1d218f258e1032114492aefa07ffa7ff4
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-21 10:04:48 +0200

    Project 3, Task 5, attempt 1

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main"
+
+/**
+* print_sign - Entry point
+* @n: is an input
+* Return: Always 0 (Success)
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		printf('0');
+		return (0);
+	}
+
+	else
+		printf('-');
+		return (-1);
+}
