6053ec9d6b2465191e0b7b61c0d61376f32cc1cb
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-21 10:27:42 +0200

    Project 3, Task 6, attempt 1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _abs - Entry point
+* @a: is an input
+* Return: Always 0 (Success)
+*/
+
+int _abs(int a);
+{
+	if (a > 0)
+	{
+		return (a);
+	}
+
+	else
+	{
+		return (-a);
+	}
+}
