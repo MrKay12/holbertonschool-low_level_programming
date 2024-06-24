3af9172d52a7ac635a23ee30e0214906f14f586b
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-24 11:13:11 +0200

    Project 4, Task 6, attempt 1

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - print
+ *
+ * @n : input
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	for (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
