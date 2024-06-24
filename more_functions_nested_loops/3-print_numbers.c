efa9a3c501053c8e633191bb74aef8bae796a6a2
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-24 09:51:33 +0200

    Project 4, Task 3, attempt 1

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_numbers - Entry point
+*
+*
+* Return: Always 0.
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
