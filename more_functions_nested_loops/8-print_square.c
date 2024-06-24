9f8d5eaad148d09386ff200eeab9fc19bc151505
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-24 14:42:29 +0200

    Project 4, Task 8, attempt 1

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	for (a = 1 ; a < size ; a++)
+	{
+		for (b = 0 ; b < size ; b++)
+			_putchar('#');
+
+		if (a != size)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
