936ba167b15cc62589ee6e42fb230407c27573f6
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-24 15:35:53 +0200

    Project 4, Task 9, attempt 12

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,22 @@ int main(void)
 
 	for (a = 1 ; a <= 100 ; a++)
 	{
-		if ((a % 3 == 0) && (a % 5 == 0))
-			printf("FizzBuzz ");
+		if (a != 100)
+			printf(" ");
+
+		else if	((a % 3 == 0) && (a % 5 == 0))
+			printf("FizzBuzz");
 
 		else if (a % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 
 		else if (a % 5 == 0)
-			printf("Buzz ");
-
-		else if (a == 100)
-			printf(" ");
+			printf("Buzz");
 
 		else
 			printf("%i ", a);
 
 	}
+	printf("\n");
 	return (0);
 }
