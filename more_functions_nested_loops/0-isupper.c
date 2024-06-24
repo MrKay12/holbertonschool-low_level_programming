16031347e1e0d64ac2183dd6845bcb78234a2a9e
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-24 09:30:01 +0200

    Project 4, Task 0, attempt 1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _isupper - Entry point
+* @c : input
+*
+* Return: Always 0.
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A'; c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
