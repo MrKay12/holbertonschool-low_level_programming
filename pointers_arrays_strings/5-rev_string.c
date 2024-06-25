5fee01417c523ba0e1914f35d8215db6ae00e8dd
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-25 18:27:00 +0200

    Project 5, Task 5, attempt 1

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - function
+ *
+ * @s: input
+ *
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+	char b;
+	int length = 0;
+	int a;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+		length++;
+
+	for (a = 0 ; a < length / 2 ; a++)
+	{
+		b = s[a];
+		s[a] = s[length - a - 1];
+		s[length - a - 1] = t;
+	}
+}
