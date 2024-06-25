0ee87a37c225371ad518a82a06d80341a820e5a4
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-25 14:53:03 +0200

    Project 5, Task 4, attempt 1

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  print_rev - fonction
+ *
+ *  @s: input
+ *
+ *  Return: always 0
+ */
+
+void print_rev(char *s)
+{
+	int a;
+	int length = 0 ;
+
+
+	for (a = length - 1; a >= 0 ; a--)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n')
+}
