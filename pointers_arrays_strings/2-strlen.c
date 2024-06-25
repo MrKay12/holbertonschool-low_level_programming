890ccd1c1683b5013b0a623a643599826c6361e5
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-25 10:23:10 +0200

    Project 5, Task 2, attempt 1

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - fonctions for pointers
+ *
+ * @s: input
+ *
+ * Return: Always return 0
+ */
+
+int _strlen(char *s)
+{
+	int a = strlen(*s);
+
+	_putchar("Length of %s = %d\n", name, length);
+	return (0);
+}
