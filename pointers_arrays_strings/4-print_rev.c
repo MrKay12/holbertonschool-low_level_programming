64ec40ddf36dfea0b0af3d581d6220e43e8a846c
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-25 15:27:36 +0200

    Project 5, Task 4, attempt 8

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -15,10 +15,13 @@ void print_rev(char *s)
 	int length = 0;
 
 	for (a = 0 ; s[a] != '\0' ; a++)
+		length++;
+	
+	length--;
 
-		for (a = length - 1; a >= 0 ; a--)
-		{
-			_putchar(s[a]);
-		}
+	for (a = length - 1; a >= 0 ; a--)
+	{
+		_putchar(s[a]);
+	}
 	_putchar('\n');
 }
