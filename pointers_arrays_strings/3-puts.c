0f96d816e8e01bdf9b35db88c71699869e2b6604
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-25 11:02:37 +0200

    Project 5, Task 3, attempt 2 (betty + killing a ;)

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -9,13 +9,13 @@
  * Return: Always 0
  */
 
-void _puts(char *str);
+void _puts(char *str)
 {
 	int a;
 
-	for (a = 0; str[a] != '\0' ; a++)
+	for (a = 0 ; str[a] != '\0' ; a++)
 	{
-	       _putchar(str[a]);
+		_putchar(str[a]);
 	}
 	_putchar('\n');
-}	
+}
