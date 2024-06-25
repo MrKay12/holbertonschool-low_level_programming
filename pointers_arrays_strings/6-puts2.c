72b736f8d0a6391e2a812d37eb892ad1d6df2a99
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-25 18:58:02 +0200

    Project 5, Task 6, attempt 1

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - function
+ *
+ * @str: pointer
+ *
+ * Return: Always 0
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0 ; str[a] != '\0' ; a++)
+	{
+		if ((a % 2) == 0)
+		{
+			_putchar(str[a]);
+			_putchar('\n');
+		}
+	}
+}
