1db4e98f5ad9080efac489319ce6a7cd13346a59
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-26 11:29:21 +0200

    Project 5, Task 7, attempt 5

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -4,19 +4,29 @@
 /**
  * puts_half - function
  *
- * @str: pointer
+ * @str: input
  *
  * Return: Always 0
  */
 
-void puts_half(char *str)
+void rev_string(char *s)
 {
 	int a;
-	;
+	int length = 0;
+	int start = 0;
 
+	for (a = 0 ; s[a] != '\0' ; a++)
+		length++;
 
-	for (a = str / 2 ; a < str ; a++)
+	if ((length % 2) == 1)
+		start = length / 2;
+
+	else
+		start = length / 2;
+	
+	for (a = start ; a < length ; a++)
 		_putchar(str[a]);
 	_putchar('\n');
-}
 
+
+}
