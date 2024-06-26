b8671519c31a3cbdfd4b3318141feaff95317354
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-26 16:02:33 +0200

    Project 5, Task 9, attempt 1

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - function
+ *
+ * @dest: 1st pointer
+ * @src: 2nd pointer
+ *
+ * Return: Always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+	int length = 0;
+
+	for (a = 0 ; src[a] != '\0' ; a++)
+		length++;
+
+	for (a = 0 ; a < length ; a++)
+		dest[a] = src[a};
+
+	return (dest);
+}
