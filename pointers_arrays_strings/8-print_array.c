51ebffe5712e73646e0d1fd9642c71facde7ceec
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-26 14:30:52 +0200

    Project 5, Task 8, attempt 1

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - function
+ *
+ * @a: 1st input
+ * @b: 2nd input
+ *
+ * Return: Always 0
+ */
+
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; i < n; a++)
+	{
+		_putchar("%d", *(arr + a));
+	}
+	_putchar('\n');
+}
