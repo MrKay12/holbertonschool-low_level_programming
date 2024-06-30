d7a35c96b579c03510fde613ed3528b550f3db00
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-30 16:29:52 +0200

    Project 6, Task 0, attempt 1

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncat - function
+ *
+ * @dest: 1st pointer
+ * @src: 2nd pointer
+ *
+ * Return: Always 0
+ */
+
+char *_strncat(char *dest, char *src)
+{
+	char *i = dest;
+
+	while (*d) d++;
+	while ((*d++ = *src++));
+
+	return dest
+}
