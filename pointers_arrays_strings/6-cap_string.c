777363c7e6a75d2e91524c49426c9f336799c1df
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-30 18:36:22 +0200

    need 80% too

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - check the code
+ *
+ * Description: print
+ *
+ * @s: input
+ *
+ * Return: Always 0
+ */
+
+int _atoi(char *s)
+{
+	*s = 98;
+	return (0);
+}
