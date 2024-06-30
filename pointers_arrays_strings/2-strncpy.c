6109064210575f113772b077968a953e6d4ebe7b
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-30 17:17:53 +0200

    Project 6, Task 2, attempt 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,8 +14,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *i = dest;
 
-	while (n-- > 0 (*d++ = *src++))
+	while (n-- > 0 (*i++ = *src++))
 		;
-	while (n-- > 0) *d++ = '\0';
+
+	while (n-- > 0) *i++ = '\0'
+		;
+
 	return (dest);
 }
