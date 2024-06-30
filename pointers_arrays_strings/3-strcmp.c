cbb7fb36773b7a5f2885e682e09da761913811fe
MrKay12 <quentin.lepoutre12@gmail.com>
2024-06-30 17:38:23 +0200

    Project 6, Task 3, attempt 1

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - function
+ *
+ * @s1: 1st pointer
+ * @s2: 2nd pointer
+ *
+ * Return: Always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
