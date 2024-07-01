18c58eb174e759f0f2e21bbb7098d2c36f4fcf89
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-01 11:06:21 +0200

    Project 7, Task 1, attempt 2

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,13 @@
  * Return: Always 0
  */
 
-char *_memcpy(char *dest, const char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
 	while (i < n)
 	{
-		des[i] = src[i];
+		dest[i] = src[i];
 		i++;
 	}
 
