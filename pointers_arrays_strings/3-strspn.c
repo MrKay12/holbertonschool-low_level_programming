9445cde3ba701ea939b28a355224071fb7c8dc09
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-04 10:38:26 +0200

    Project 7, Task 3, attempt 6 (test)

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -28,7 +28,41 @@ unsigned int _strspn(char *s, char *accept)
 			accept++;
 		}
 
-		if (!i)
+		if (!i)unsigned int _strspn(char s, characcept)
+{
+    unsigned int i, j, further = 0, length = 0, newLength = 0, stillGoing = 0;
+
+    for (i = 0 ; s[i] != '\0' ; i++)
+    {
+        for (j = further ; accept[j] != '\0' ; j++)
+        {
+            printf("%c, ", accept[j]);
+            if (s[i] == accept[j])
+            {
+                further = j;
+                if (stillGoing != i - 1)
+                {
+                    if (newLength > length)
+                        length = newLength;
+                    newLength = 0;
+                    further = 0;
+                }
+                stillGoing = i;
+                newLength++;
+                printf("\n");
+                break;
+            }
+        }
+        if (accept[j] == '\0')
+        {
+            further = 0; 
+            if (newLength > length)
+		    length = newLength;
+	    newLength = 0;
+	}
+    }
+	return (length);
+}
 		{
 			break;
 		}
