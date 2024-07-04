44f2e8042e45f5bb7ac838b81ca5d6b91ed4b362
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-04 14:35:06 +0200

    Project 9, Task 0, attempt 4

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
+ * main - function
  *
+ *@argc: argc
+ *@argv: pointer
  *
- *
- *
- *
+ *Return: Always 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
-	
+
 	return (0);
 }
