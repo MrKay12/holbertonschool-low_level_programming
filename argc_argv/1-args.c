db3c8c4f37a182f2835c5541475fa0cd7b533161
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-04 14:44:20 +0200

    Project 9, Task 1, attempt 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ *@argc: argc
+ *@argv: pointer
+ *
+ *Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
