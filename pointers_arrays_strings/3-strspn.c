69ee6b84b56d3a87aef521f93ade2ef0f6bbeafc
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-04 10:48:35 +0200

    Project 7, Task 3, attempt 7 (BETTY)

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,34 +11,35 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j, further = 0, length = 0, newLength = 0, stillGoing = 0;
+	unsigned int i, j, further = 0, length = 0, newLength = 0, stillGoing = 0;
 
-    for (i = 0 ; s[i] != '\0' ; i++)
-    {
-        for (j = further ; accept[j] != '\0' ; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                further = j;
-                if (stillGoing != i - 1)
-                {
-                    if (newLength > length)
-                        length = newLength;
-                    newLength = 0;
-                    further = 0;
-                }
-                stillGoing = i;
-                newLength++;
-                break;
-            }
-        }
-        if (accept[j] == '\0')
-        {
-            further = 0; 
-            if (newLength > length)
-		    length = newLength;
-	    newLength = 0;
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = further ; accept[j] != '\0' ; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				further = j;
+				if (stillGoing != i - 1)
+				{
+					if (newLength > length)
+						length = newLength;
+					newLength = 0;
+					further = 0;
+				}
+				stillGoing = i;
+				newLength++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			further = 0;
+
+		if (newLength > length;
+			length = newLength;
+			newLength = 0;
 	}
-    }
+	}
 	return (length);
 }
