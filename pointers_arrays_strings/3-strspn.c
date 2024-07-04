8d374876d295c18949bbeb69888e2dbff40cff72
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-04 10:03:50 +0200

    Project 7, Task 3, attempt 5

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -13,20 +13,19 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0;
 	unsigned int j = 0;
-	char **a = *accept;
 
 	while (*s)
 	{
 
-		while (**a)
+		while (*accept)
 		{
-			if (*s == **a)
+			if (*s == *accept)
 			{
 				i = 1;
 				break;
 			}
 
-			a++;
+			accept++;
 		}
 
 		if (!i)
