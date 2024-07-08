0de120a0449b222204fd8599c443eb5a14bd5125
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-08 14:28:59 +0200

    Project 1, Task 1, attempt 7

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,15 +13,17 @@
 
 char *_strdup(char *str)
 {
-	int i;
-	int j = 0;
-	char *s = malloc(j++ * sizeof(char));
-
-	while (str[j] != '\0')
-		j++;
+	unsigned int i;
+	unsigned int j = 0;
+	char *s;
 
 	if (str == NULL)
 		return (NULL);
+	
+	while (str[j] != '\0')
+		j++;
+
+	s = (char *)malloc((j + 1) * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
