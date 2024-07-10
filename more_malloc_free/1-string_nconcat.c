eb9c4ad42ac8f0e25889b426be134ba39f9552fc
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-10 15:37:45 +0200

    Project 2, Task 1, attempt 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * string_nconcat - funtion
+ *
+ * @s1: 1st pointer
+ * @s2: 2nd pointer
+ * @n: var
+ *
+ *Return: Always 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int k = 0;
+	unsigned int l = 0;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[k] != '\0')
+		k++;
+
+	while (s2[l] != '\0')
+		l++;
+
+	if (n >= l)
+		n = l;
+
+	s = (char *)malloc(k + n + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < k; i++)
+		s[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		s[k + j] = s2[j];
+unsigned
+	s[k + n] = '\0';
+
+	return (s);
+}
