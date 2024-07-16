93251f9dde914a2b2fbd7656c12fcb8330adc482
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-16 14:42:10 +0200

    Project 1, Task 3, (required for 3rd task 2)

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "calc.h"
+
+/**
+ * get_op_func: function
+ *
+ * @s: pointer
+ *
+ * Return: Always 0
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	while (i < 5)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
