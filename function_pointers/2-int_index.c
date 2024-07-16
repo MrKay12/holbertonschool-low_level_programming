afb2472d093b0516f780604888c4493d8514a790
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-16 09:41:07 +0200

    Project 1, Task 2, attempt 2 (swap order)

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -2,27 +2,27 @@
 #include "function_pointers.h"
 
 /**
+ * int_index - function
  *
+ *@array: 1st pointer
+ *@cmp: 2nd pointer
+ *@size : var
  *
- *
- *
- *
- *
- *
+ * Retrun: Always 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
+	if (array == NULL || cmp == NULL || size <= 0)
+                return (-1);
+
 	for (i = 0 ; i < size ; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
 	}
-
-	if (array == NULL || cmp == NULL || size <= 0)
-		return (-1);
 
 	return (-1);
 }
