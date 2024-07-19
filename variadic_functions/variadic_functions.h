cb44e8e71fac351ba289bd19c434f74bb236678c
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-19 15:40:27 +0200

    update for task 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,3 +1,6 @@
+#include <stdarg.h>
+#include <stdio.h>
+
 #ifndef main_h
 #define main_h
 
@@ -7,6 +10,26 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct format - typedef
+ *
+ * @type: char
+ * @f: function
+ *
+ */
+
+typedef struct format
+{
+	char type;
+	void (*f)(va_list);
+} fm;
+
+void print_all(const char * const format, ...);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
 
 
 #endif
