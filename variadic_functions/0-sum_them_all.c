0d7eabd9ad621ed6dc3f4cc5ce9f58faffddb92e
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-19 09:58:49 +0200

    Project 2, Task 0, attempt 1

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function
+ *
+ * @n: var
+ *
+ * Return: Always 0 
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list num;
+
+	if (n == 0)
+		return (0);
+
+	va_start(num, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(num, int);
+
+	va_end(num);
+
+	return (sum);
+}
