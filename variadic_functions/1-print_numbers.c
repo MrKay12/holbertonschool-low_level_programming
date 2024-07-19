a069249035c84e04acf42470829cb51bbdf6fd14
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-19 10:51:13 +0200

    Project 2, Task 1, attempt 2

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,10 +19,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(p, n);
 
 	for (i = 0 ; i < n ; i++)
+	{
 		printf("%d", va_arg(p, int));
-
-	if (separator != NULL && i < n - 1)
-		printf("%s", separator);
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
 
 	va_end(p);
 
