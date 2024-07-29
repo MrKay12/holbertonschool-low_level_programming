74ac4d8f5da7992ebb3a8c3ab7d14a5ae2cc564d
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-29 10:58:13 +0200

    Project 1, Task 0, attempt 1

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nc = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%u] %s\n",h->len, h->str);
+
+		h = h->next;
+		nc++;
+	}
+
+	return (nc);
+}
