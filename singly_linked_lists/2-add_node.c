b53f4449942c419577bc4d46b498b2d13bcef3db
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-29 13:49:59 +0200

    Project 1, Task 2, attempt 1

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_node - function
+ *
+ *@head: double pointer
+ *@str: pointer
+ *
+ *Return: the new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n;
+	char *nstr;
+	unsigned int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	nstr = strdup(str);
+
+	if (nstr == NULL)
+		return (NULL);
+
+	n = malloc(sizeof(list_t));
+
+	if (n == NULL)
+		return (NULL);
+
+	n->str = nster;
+	n->len = len;
+	n->next = *head;
+
+	*head = n;
+
+	return (n);
+}
