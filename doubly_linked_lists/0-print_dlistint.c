4f3b696b48aa6c568d985ef5939e8b5392dee71f
MrKay12 <quentin.lepoutre12@gmail.com>
2024-08-01 10:05:05 +0200

    Project 2, Task 0, attempt 1

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function
+ *
+ *@h: pointer
+ *
+ *Return: Always 0
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
