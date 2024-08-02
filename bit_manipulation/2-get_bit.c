92f592b6009033e8e92505c7fa0e59b985e181d2
MrKay12 <quentin.lepoutre12@gmail.com>
2024-08-02 14:25:51 +0200

    Project 3, Task 2, attempt 1.5 (Betty)

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
+ * get_bit - function
  *
+ *@n: var
+ *@index: index
  *
- *
- *
- *
- *
+ *Return: Always 0
  */
 
 int get_bit(unsigned long int n, unsigned int index)
@@ -14,5 +14,5 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index >= sizeof(n) * 8)
 		return (-1);
 
-	return (n >> index) & 1;
+	return ((n >> index) & 1);
 }
