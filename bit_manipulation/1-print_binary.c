4146b36295f412afaed97520252f47b57154b61c
MrKay12 <quentin.lepoutre12@gmail.com>
2024-08-02 14:01:03 +0200

    Project 3, Task 1, attempt 2 (now i corectly read instruction)

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -10,8 +10,23 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n >> 1)
-		print_binary(n >> 1);
+	int b = sizeof(n) * 8 - 1;
+	int i = 1;
 
-	printf("%lu", n & 1);
+	while (b >= 0)
+	{
+		if ((n >> b) & 1)
+		{
+			printf("1");
+			i = 0;
+		}
+
+		else if (!i)
+			printf("0");
+
+		b--;
+	}
+
+	if (i)
+		printf("0");
 }
