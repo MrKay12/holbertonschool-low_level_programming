f2a098b903f185a60c84bd5901ba148bdc136e0d
MrKay12 <quentin.lepoutre12@gmail.com>
2024-08-02 10:32:41 +0200

    Project 2, Task 7, attempt 2

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,12 +13,13 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *nn, *i;
-	unsigned int j;
+	unsigned int j = 0;
+
+	if (h == NULL)
+                return (NULL);
 
 	nn = malloc(sizeof(dlistint_t));
 
-	if (h == NULL)
-		return (NULL);
 	if (nn == NULL)
 		return (NULL);
 
