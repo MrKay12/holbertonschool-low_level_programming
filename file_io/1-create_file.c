c5db711ec74c60972f470dd7f3a09ff6306f40ab
MrKay12 <quentin.lepoutre12@gmail.com>
2024-08-04 11:25:04 +0200

    Project 4, Task 1, attempt 1 (on my way to be lost)

---
+++ file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include <sys/stat.h>
+#include "main.h"
+
+/**
+ * create_file - function
+ *
+ *@filename: all in the name
+ *@text_content: same thing
+ *
+ *Return: Always
+ */
+
+
+int create_file(const char *filename, char *text_content)
+{
+	int d;
+	ssize_t w;
+
+	if (filename == NULL)
+		return (0);
+
+	d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (d == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		w = write(d, text_content, strlen(text_content));
+		if (w == -1)
+		{
+			close(d);
+			return (-1);
+		}
+	}
+
+	close(d);
+	return (1);
+}
